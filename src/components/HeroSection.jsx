8e3699fb9fdaf73dcc27304ff57df9cbdadfc7a1
Adarsh Kumar <76390366+adarsh206@users.noreply.github.com>
2024-04-09 13:33:14 +0530

    virtual reality landing page

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,43 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import video1 from "../assets/video1.mp4";
+import video2 from "../assets/video2.mp4";
+
+
+const HeroSection = () => {
+  return (
+    <div className="flex flex-col items-center mt-6 lg:mt-20">
+        <h1 className='text-4xl sm:text-6xl lg:text-7xl text-center tracking-wide'>
+           Virtual Build tools
+            <span className='bg-gradient-to-r from-green-500 to-red-800 text-transparent bg-clip-text'>
+                  {" "} 
+                  for developers   
+            </span>
+        </h1>
+        <p className='mt-10 text-lg text-center text-neutral-500 max-w-4xl'>
+            Empower your creativity and bring your VR app ideas to life with our intuitive development tools.
+            Get started today and turn your imagination into immersive reality!
+        </p>
+        <div className='flex justify-center my-10'>
+            <a href='#' className='bg-gradient-to-r from-green-500 to-red-800 py-3 px-4 mx-3 rounded-md'>
+                Start for free
+            </a>
+            <a href='#' className='py-3 px-4 mx-3 rounded-md border'>
+                Documentation
+            </a>
+        </div>
+        <div className='flex mt-10 justify-center'>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-green-700 border-e-orange-400 border-b-orange-400 shadow-orange-400 mx-2 my-4'>
+                <source src={video2} type='video/mp4' />
+                Your browser does not support the video tag.
+            </video>
+            <video autoPlay loop muted className='rounded-lg w-1/2 border border-green-700 border-s-orange-400 border-b-orange-400 shadow-orange-400 mx-2 my-4'>
+                <source src={video1} type='video/mp4' />
+                Your browser does not support the video tag.
+            </video>
+        </div>
+    </div>
+  )
+}
+
+export default HeroSection
